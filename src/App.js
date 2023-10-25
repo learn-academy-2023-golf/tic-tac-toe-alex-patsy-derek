c7c4a48bff725e07b704bb001035c5b92d1c0019
Derek Maly <dmaly18@gmail.com>
2023-10-25 11:09:26 -0700

    made it to where if you click on a box it marks it

--- src/App.js
+++ src/App.js
@@ -5,22 +5,36 @@ import './App.css'
 const App = () => {
   const [squares, setSquares] = useState(Array(9).fill(null))
 
+  // Function to handle a square click
+  const handleSquareClick = (index) => {
+    // Create a copy of the squares array to avoid mutating state directly
+    const newSquares = [...squares]
+    
+    // Check if the square is already filled or if the game is over
+    if (newSquares[index] === null) {
+      // Set the square to 'X' or 'O' based on the current player
+      newSquares[index] = 'X' // You can alternate 'X' and 'O' for each player's turn
+
+      // Update the state with the new squares array
+      setSquares(newSquares)
+    }
+  }
+
   return (
     <>
       <h1>Tic Tac Toe</h1>
       <div className='gameBoard'>
-      {squares.map((value, index) => {
-        return(
-      <Square 
-            value={value} 
+        {squares.map((value, index) => (
+          <Square
+            value={value}
             key={index}
-            index={index}
-      />
-      )
-    })}
+            onClick={() => handleSquareClick(index)} // Pass the click handler
+          />
+        )
+        )}
       </div>
     </>
   )
 }
 
-export default App
+export default App;
