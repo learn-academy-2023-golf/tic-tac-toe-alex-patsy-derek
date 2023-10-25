2d74424bed9f3404ccf64c7a91ecf321d291e1c1
Derek Maly <dmaly18@gmail.com>
2023-10-25 14:48:02 -0700

    created alternate X, O,

--- src/App.js
+++ src/App.js
@@ -6,14 +6,29 @@ const App = () => {
   const [squares, setSquares] = useState(Array(9).fill(null))
   const [player, setPlayer] = useState('X')
 
+  const calculateWinner = (squares) => {
+    const lines = [
+      [0, 1, 2], [3, 4, 5], [6, 7, 8], 
+      [0, 3, 6], [1, 4, 7], [2, 5, 8], 
+      [0, 4, 8], [2, 4, 6]
+    ]
+
+    return lines.some(([a, b, c]) => squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) || null
+  }
+  
+
   const handleSquareClick = (index) => {
-     if (squares[index] === null) {
+     if (squares[index] === null && !calculateWinner(squares)) {
       const newSquares = [...squares]
       newSquares[index] = player
       setSquares(newSquares)
+      const winner = calculateWinner(newSquares)
+      if (winner) {
+        alert(`Player wins!`)
+      } else {
       setPlayer(player === 'X' ? 'O' : 'X')
+     }
     }
-
   }
 
   return (
